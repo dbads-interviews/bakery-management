2a4f2b0f4fabb429d09150feeb6aed9c53a52612
Deepak Bharti <deepakbharti@mnnit.ac.in>
2021-03-01 02:09:53 +0530

    password field

--- api/user/serializers.py
+++ api/user/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+  password = serializers.CharField(
+      style={'input_type': 'password'},
+      min_length=6,
+      max_length=68,
+      write_only=True)
+
   class Meta:
     model = User
     fields = ['username', 'password', 'id']
