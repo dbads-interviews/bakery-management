e5957d4eda8c7a2828b8674367aee34806dc756d
Deepak Bharti <deepakbharti@mnnit.ac.in>
2021-02-28 19:52:01 +0530

    added view for create/get/list/delete/update users

--- api/users/views.py
+++ api/users/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from rest_framework import generics, mixins
+from .serializers import UserSerializer
 # Create your views here.
 
 # def create_user(request):
 #   # create user
+
+
+class UserGenericAPIView(generics.GenericAPIView,
+                         mixins.CreateModelMixin, mixins.ListModelMixin,
+                         mixins.DestroyModelMixin, mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin):
+  serializer_class = UserSerializer
+  queryset = User.objects.all()
+  lookup_field = 'id'
+
+  def get(self, request, id=None):
+    if id:
+      return self.retrieve(request)
+    else:
+      return self.list(request)
+
+  def post(self, request):
+    return self.create(request)
+
+  def put(self, request, id=None):
+    return self.update(request, id)
+
+  def delete(self, request):
+    return self.destroy(request, id)
