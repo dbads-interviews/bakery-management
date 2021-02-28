b5efc8918aa9b4cf12222b1425053ba1fa8dbd95
Deepak Bharti <deepakbharti@mnnit.ac.in>
2021-02-28 20:47:10 +0530

    commented generic api views and using modelViewset

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import UserGenericAPIView
+from django.urls import path, include
+# from .views import UserListGenericAPIView, UserDetailGenericAPIView, UsersViewset
+from .views import UsersViewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('', UsersViewset, basename='users')
 
 urlpatterns = [
-    path('/', UserGenericAPIView.as_view(), name='users'),
-    path('/<int:id>', UserGenericAPIView.as_view(), name='user_detail')
+    path('/', include(router.urls)),
+    # path('/', UserListGenericAPIView.as_view(), name='user_list'),
+    # path('/<int:id>', UserDetailGenericAPIView.as_view(), name='user_detail'),
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, mixins, viewsets
+# from rest_framework import generics, mixins, viewsets
+from rest_framework import viewsets
 from .serializers import UserSerializer
 # Create your views here.
 
@@ -7,33 +8,38 @@ from .serializers import UserSerializer
 #   # create user
 
 
-class UserListGenericAPIView(
-        generics.GenericAPIView,
-        mixins.CreateModelMixin, mixins.ListModelMixin):
+# class UserListGenericAPIView(
+#         generics.GenericAPIView,
+#         mixins.CreateModelMixin, mixins.ListModelMixin):
+#   serializer_class = UserSerializer
+#   queryset = User.objects.all()
+#   lookup_field = 'id'
+
+#   def get(self, request):
+#     return self.list(request)
+
+#   def post(self, request):
+#     return self.create(request)
+
+
+# class UserDetailGenericAPIView(
+#         generics.GenericAPIView,
+#         mixins.DestroyModelMixin, mixins.RetrieveModelMixin,
+#         mixins.UpdateModelMixin):
+#   serializer_class = UserSerializer
+#   queryset = User.objects.all()
+#   lookup_field = 'id'
+
+#   def get(self, request, id=None):
+#     return self.retrieve(request)
+
+#   def put(self, request, id=None):
+#     return self.update(request, id)
+
+#   def delete(self, request, id=None):
+#     return self.destroy(request, id)
+
+
+class UsersViewset(viewsets.ModelViewSet):
   serializer_class = UserSerializer
   queryset = User.objects.all()
-  lookup_field = 'id'
-
-  def get(self, request):
-    return self.list(request)
-
-  def post(self, request):
-    return self.create(request)
-
-
-class UserDetailGenericAPIView(
-        generics.GenericAPIView,
-        mixins.DestroyModelMixin, mixins.RetrieveModelMixin,
-        mixins.UpdateModelMixin):
-  serializer_class = UserSerializer
-  queryset = User.objects.all()
-  lookup_field = 'id'
-
-  def get(self, request, id=None):
-    return self.retrieve(request)
-
-  def put(self, request, id=None):
-    return self.update(request, id)
-
-  def delete(self, request, id=None):
-    return self.destroy(request, id)
