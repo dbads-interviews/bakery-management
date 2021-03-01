c8bc4e3e14a08b4cd5496a47e0374f3db3952413
Deepak Bharti <deepakbharti@mnnit.ac.in>
2021-03-01 21:23:29 +0530

    custom user model

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+  username = None
+  email = models.EmailField('email address', unique=True)
+  first_name = models.CharField('First Name', max_length=255, blank=True,
+                                null=False)
+  last_name = models.CharField('Last Name', max_length=255, blank=True,
+                               null=False)
+  role = models.IntegerField(default=0)  # 0=customer, 1=admin
+
+  USERNAME_FIELD = 'email'
+
+  def __str__(self):
+    return f"{self.email} - {self.first_name} {self.last_name}"
