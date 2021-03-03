a223ef84290ab1aa22065bbc60384ee343950a9e
Deepak Bharti <deepakbharti@mnnit.ac.in>
2021-03-03 23:47:44 +0530

    only staff users can perform crud on products/ingredients

--- api/product/views.py
+++ api/product/views.py
@@ -1,11 +1,15 @@
 from .models import Product
 # from rest_framework import generics, mixins, viewsets
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .serializers import ProductSerializer
 
 
 class ProductViewset(viewsets.ModelViewSet):
   serializer_class = ProductSerializer
   queryset = Product.objects.all()
-  permission_classes = [IsAuthenticated]
+  permission_classes = [IsAuthenticated, IsAdminUser, ]
+
+  def list(self, request):
+    print(self.request.user, self.request.user.is_staff)
+    return super().list(self, request)
